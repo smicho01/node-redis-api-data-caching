bb49eedc0fa954511c18d131d1a2de3cb34c61eb
Seweryn Michota <Seweryn.Michota@digital.homeoffice.gov.uk>
2022-04-28 23:26:11 +0100

    Wokring Redis caching example - no exiration time for cache yet

--- api.js
+++ api.js
@@ -1,13 +1,35 @@
 import axios from "axios";
+import Redis from "ioredis";
 
+
+const redis = new Redis({
+    'port': 6379,
+    'host': '127.0.0.1'
+})
 const API_KEY = '67cdf2b79e9ca4fe79b9f0b145f59f1e'
 const cityEndpoint = (city) => `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`
 
 const getWeather = async (city) => {
+    // Try if value exists in Redis cache & use it
+    let cahceEntry = await redis.get(`weather:${city}`)
+    // If Redist has that cached (cahce hit) ... return it
+    if(cahceEntry) {
+        cahceEntry = JSON.parse(cahceEntry)
+        return {...cahceEntry, 'source' : 'Redis Cache'}
+    }
+    
+    // ... No cache (cache miss) .... then get response from API
     let apiResponse = await axios.get(cityEndpoint(city))
+    redis.set(`weather:${city}`, JSON.stringify(apiResponse.data))
     return {...apiResponse.data, 'source' : 'API'}
 }
 
 const city = 'London'
+const timeStart = new Date().getTime() // to calculate response time
 let weather = await getWeather(city)
+const timeEnd = new Date().getTime() // to calculate response time
+
+weather.responseTime = `${timeEnd - timeStart}ms`
+
 console.log(weather)
+process.exit()
